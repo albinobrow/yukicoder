492763162588ef7d800b0bd93507b93babb7085c
albinobrow <wtasami@hotmail.com>
2018-12-24 14:38:05 +0900

    Add files via upload

---
+++ t.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+n,k=[int(input()) for i in range(2)]
+l=sorted(list(int(input()) for i in range(n)))
+if k > 2:
+	print(int(round(l[-1]-l[0])))
+else:
+	m=-float("inf")
+	for i in range(1,n):
+		a=np.mean(l[:i])
+		b=np.mean(l[i:])
+		c=int(round(b-a))
+		if m < c:
+			m=c
+	print(m)
